9e451deb4018818eb5e54af95d797cf016951394
Abraham Smith <abe@abesmith.co.uk>
2022-02-18 17:25:27 +0100

    update segment_patch to use ssl/tcp instead of scp

--- trainer/patch_seg.py
+++ trainer/patch_seg.py
@@ -1,5 +1,6 @@
 """ Segment just one patch
 Copyright (C) 2021 Abraham George Smith
+Copyright (C) 2022 Abraham George Smith
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,10 +18,17 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import os
 import time
 import torch
+import socket
+import ssl
+import numpy as np
+import zlib
+import json
+import rp_annot as rpa
 
 from skimage import img_as_float32
 import numpy as np
 from torch.nn.functional import softmax
+from instructions import fix_config_paths
 
 from startup import add_config_shape
 import im_utils
@@ -33,39 +41,56 @@ cached_model = None
 cached_model_path = None
  
 
-def segment_patch(segment_config):
+def start_server(sync_dir, ip, port):
+    server_cert = os.path.join(Path.home(), 'root_painter_server.public_key')
+    server_key = os.path.join(Path.home(), 'root_painter_server.private_key')
+    client_certs = os.path.join(Path.home(), 'root_painter_client.public_key')
+
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+    context.load_verify_locations(cafile=client_certs)
+
+    # Create a TCP/IP socket
+    # Listen for incoming connections
+    sock = socket.socket()
+    print(f'Listening on {ip} port {port}')
+    sock.bind((ip, port))
+    sock.listen(1)
+    while True:
+        conn, client_address = sock.accept()
+        conn = context.wrap_socket(conn, server_side=True)
+        print("SSL established. Peer: {}".format(conn.getpeercert()))
+        print('connection from', client_address)
+        annot = None
+        buf = b''
+        while True:
+            data = conn.recv(16)
+            if data == b'end':
+                shape = np.frombuffer(buf[:16], dtype='int32')
+                annot1d = rpa.decompress(buf[16:], np.prod(shape))
+                annot = annot1d.reshape(shape)
+                buf = b''
+            elif data == b'cfg':
+                segment_config = json.loads(buf.decode())
+                segment_config = fix_config_paths(sync_dir, segment_config)
+                segment_patch(segment_config, annot, conn)
+                buf = b''
+            else:
+                buf += data
+
+
+def segment_patch(segment_config, annot_patch, conn):
     global cached_image
     global cached_image_fname
     global cached_model
     global cached_model_path
- 
+
     start = time.time()
-    # in_dir, seg_dir, fname, model_paths,
-    # in_w, out_w, in_d, out_d, class_name
-    """
-    Segment {file_names} from {dataset_dir} using {model_paths}
-    and save to {seg_dir}.
-
-    If model paths are not specified then use
-    the latest model in {model_dir}.
-
-    If no models are in {model_dir} then create a
-    random weights model and use that.
-
-    TODO: model saving is a counter-intuitve side effect,
-    re-think project creation process to avoid this
-    """
-
     fname = segment_config['file_name']
-    patch_annot_fname = segment_config['patch_annot_fname']
     dataset_dir = segment_config['dataset_dir']
-
     model_dir = segment_config['model_dir']
-    seg_dir = segment_config['seg_dir']
-
-    patch_annot_dir = segment_config['patch_annot_dir']
     classes = segment_config['classes']
-   
     # Where do we take the patch from? (coord dont include padding)
     x_start = segment_config['x_start']
     y_start = segment_config['y_start']
@@ -73,10 +98,6 @@ def segment_patch(segment_config):
     x_end = segment_config['x_end']
     y_end = segment_config['y_end']
     z_end = segment_config['z_end']
-
-    client_ip = segment_config['client_ip']
-    client_username = segment_config['client_username']
-
     model_path = get_latest_model_paths(model_dir, 1)[0]
     fpath = os.path.join(dataset_dir, fname)
 
@@ -111,11 +132,9 @@ def segment_patch(segment_config):
     model_input = torch.cuda.FloatTensor(1, 3, im_patch.shape[0], im_patch.shape[1], im_patch.shape[2])
     model_input[0, 0] = torch.from_numpy(im_patch.astype(np.float32)).cuda()
 
-    # load the annotation, as this must also be used as input to the network (for optimal results)
-    annot_fpath = os.path.join(patch_annot_dir, patch_annot_fname)
-    annot_patch = im_utils.load_image(annot_fpath)
     bg_patch = annot_patch[0]
     fg_patch = annot_patch[1]
+
     model_input[0, 1] = torch.from_numpy(fg_patch).cuda()
     model_input[0, 2] = torch.from_numpy(bg_patch).cuda()
 
@@ -131,7 +150,6 @@ def segment_patch(segment_config):
     # bg channel index for each class in network output.
     class_idxs = [x * 2 for x in range(outputs.shape[1] // 2)]
     class_output_patches = [[] for _ in class_idxs]
-
     for i, class_idx in enumerate(class_idxs):
         # class_output = (batch_size, bg/fg, depth, height, width)
         class_output = outputs[:, class_idx:class_idx+2]
@@ -142,13 +160,11 @@ def segment_patch(segment_config):
         pred_np = predicted.data.detach().cpu().numpy()
         for out_tile in pred_np:
             class_output_patches[i].append(out_tile)
-
-    seg_fname = patch_annot_fname.replace('.npy', '.npz')
-    out_path = os.path.join(seg_dir, seg_fname)
     # For now only the first class will be segmented 
     seg = class_output_patches[0][0]
     # send segmented region to client
-    scp_transfer(seg, seg_fname, client_ip, client_username)
+    seg = seg.astype(bool)
+    compressed = rpa.compress(seg.reshape(-1))
+    conn.sendall(compressed)
+    conn.sendall(b'end')
     print('time to segment patch and transfer', time.time() - start)
-    # delete the annotation patch. dont want to use up all the disk.
-    os.remove(annot_fpath)
