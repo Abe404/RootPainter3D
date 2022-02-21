dae4a2eb8834eaf1091ce25f8b6e646e6f522f51
Abraham George Smith <abe@abesmith.co.uk>
2022-02-21 15:39:47 +0100

    implement PatchSegmentor class

--- painter/src/main/python/patch_seg.py
+++ painter/src/main/python/patch_seg.py
@@ -14,9 +14,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+from logging import root
 import os
 import time
 import numpy as np
+from functools import partial
 from PyQt5 import QtCore
 
 from tcp_utils import request_patch_seg
@@ -25,26 +27,80 @@ from instructions import fix_instruction_paths
 
 class SegmentPatchThread(QtCore.QThread):
 
+    complete = QtCore.pyqtSignal(np.ndarray)
 
-    def __init__(self,  x, y, z, root_painter):
+    def __init__(self, annot_patch, content, server_ip, server_port):
          super().__init__()
-         self.x = x
-         self.y = y
-         self.z = z
-         self.root_painter = root_painter
+         self.annot_patch = annot_patch
+         self.content = content
+         self.server_ip = server_ip
+         self.server_port = server_port
 
     def run(self):
+        patch_seg = request_patch_seg(self.annot_patch,
+                                      self.content,
+                                      self.server_ip,
+                                      self.server_port)
+        self.complete.emit(patch_seg)
 
-        x = self.x
-        y = self.y
-        z = self.z
+
+class PatchSegmentor():
+    """ 
+    Holds the reference to root_painter
+    and a reference to a QThread object that
+    handles the server request to get the segmented patch.
+    Then updates the root_painter segmentation data and 
+    and triggers re-render.
+    """
+
+    def __init__(self, root_painter):
+        self.root_painter = root_painter
+
+    def patch_received(self, patch_seg, bounded_fname):
+
+        # only update if the image is still the same
+        if self.root_painter.bounded_fname == bounded_fname:
+            # TODO: I know 17 from memory. Compute '17' by 
+            #       comparing input and output size
+            seg_start_x = self.x_start + 17
+            seg_start_y = self.y_start + 17
+            seg_start_z = self.z_start + 17
+            # first copy the seg data.
+            new_seg_data = np.array(self.root_painter.seg_data)
+            # update based on new patch data
+            new_seg_data[
+                seg_start_z:seg_start_z+patch_seg.shape[0],
+                seg_start_y:seg_start_y+patch_seg.shape[1],
+                seg_start_x:seg_start_x+patch_seg.shape[2]
+            ] = patch_seg
+            # now only update the displayed seg data based on the region the user intended to update
+            self.root_painter.seg_data[
+                self.z_valid_min:self.z_valid_max] = new_seg_data[self.z_valid_min:self.z_valid_max]
+            for v in self.root_painter.viewers:
+                if v.isVisible():
+                    v.update_seg_slice()
+                    v.update_outline()
+            print('patch seg duration = ', time.time() - self.start_time)
+
+    def segment_patch(self, centroid_x, centroid_y, z):
+        """
+        Prepare the data from the annotation and passes only the essentials.
+        to a thread that uses a TCP SSL socket to send the patch details to the server
+        and get back the patch segmentation.
+
+        When the segmentation is received from the server, the patch_received event handler
+        will be invoked.
+        """
+        self.start_time = time.time()
+        x = centroid_x
+        y = centroid_y
+        z = z
         root_painter = self.root_painter
-
         run_start = time.time()
-        z = z + 1 # we dont segment the current slice. It gets confusing.
+        #z = z + 1 # we dont segment the current slice. It gets confusing.
         # keep track of the region the user wanted to update. We dont allow changes outside of this region.
-        z_valid_min = z
-        z_valid_max = z_valid_min + root_painter.output_shape[0]
+        self.z_valid_min = z
+        self.z_valid_max = self.z_valid_min + root_painter.output_shape[0]
 
         # Move the centroid of the predicted region down by half the output depth
         # That way the whole output has a chance to be in the valid region.
@@ -66,22 +122,26 @@ class SegmentPatchThread(QtCore.QThread):
 
         # and not more than the image shape
         # as only centroid is supplied, we must calculate left and right.
-        z_start =  z - (root_painter.input_shape[0] // 2)
-        z_end = z_start + root_painter.input_shape[0]
-        y_start = y - (root_painter.input_shape[1] // 2)
-        y_end = y_start + root_painter.input_shape[1]
-        x_start = x - (root_painter.input_shape[2] // 2)
-        x_end = x_start + root_painter.input_shape[2]
+        self.z_start =  z - (root_painter.input_shape[0] // 2)
+        z_end = self.z_start + root_painter.input_shape[0]
+        self.y_start = y - (root_painter.input_shape[1] // 2)
+        y_end = self.y_start + root_painter.input_shape[1]
+        self.x_start = x - (root_painter.input_shape[2] // 2)
+        x_end = self.x_start + root_painter.input_shape[2]
 
         start_time = time.time()
+        
         content = {
-            "file_name": root_painter.bounded_fname,
+            # required to know which image to load
+            "file_name": root_painter.bounded_fname, 
             "dataset_dir": os.path.join(root_painter.proj_location, 'bounded_images'),
+            # required to know where to load the model from
             "model_dir": root_painter.model_dir,
-            "z_start": z_start, "z_end": z_end,
-            "y_start": y_start, "y_end": y_end,
-            "x_start": x_start, "x_end": x_end,
-            "patch_annot_dir": os.path.join(root_painter.proj_location, 'patch', 'annotation'),
+            # which region of the image to segment
+            "z_start": self.z_start, "z_end": z_end,
+            "y_start": self.y_start, "y_end": y_end,
+            "x_start": self.x_start, "x_end": x_end,
+            # which classes to use with this model
             "classes": root_painter.classes
         }
         content = fix_instruction_paths(content, root_painter.sync_dir)
@@ -90,14 +150,14 @@ class SegmentPatchThread(QtCore.QThread):
         #       First dimension is for bg (0) and fg (1)
 
         # if annotation goes outside the image, then we must pad it.
-        pad_z_start = min(0, z_start) * -1
-        pad_y_start = min(0, y_start) * -1
-        pad_x_start = min(0, x_start) * -1 
+        pad_z_start = min(0, self.z_start) * -1
+        pad_y_start = min(0, self.y_start) * -1
+        pad_x_start = min(0, self.x_start) * -1 
         
         annot_patch = root_painter.annot_data[:,
-                                              max(0, z_start):z_end,
-                                              max(0, y_start):y_end,
-                                              max(0, x_start):x_end]
+                                              max(0, self.z_start):z_end,
+                                              max(0, self.y_start):y_end,
+                                              max(0, self.x_start):x_end]
         annot_patch = annot_patch.astype(bool)
         pad_z_end = root_painter.input_shape[0] - (annot_patch.shape[1] + pad_z_start)
         pad_y_end = root_painter.input_shape[1] - (pad_y_start + annot_patch.shape[2])
@@ -111,27 +171,9 @@ class SegmentPatchThread(QtCore.QThread):
                                 (pad_x_start, pad_x_end)],
                                 mode='constant')
 
-        patch_seg = request_patch_seg(annot_patch, content,
-                                      root_painter.server_ip,
-                                      root_painter.server_port)
-
-        # TODO: I know 17 from memory. Compute '17' by 
-        #       comparing input and output size
-        seg_start_x = x_start + 17
-        seg_start_y = y_start + 17
-        seg_start_z = z_start + 17
-        # first copy the seg data.
-        new_seg_data = np.array(root_painter.seg_data)
-        # update based on new patch data
-        new_seg_data[
-            seg_start_z:seg_start_z+patch_seg.shape[0],
-            seg_start_y:seg_start_y+patch_seg.shape[1],
-            seg_start_x:seg_start_x+patch_seg.shape[2]
-        ] = patch_seg
-        # now only update the displayed seg data based on the region the user intended to update
-        root_painter.seg_data[z_valid_min:z_valid_max] = new_seg_data[z_valid_min:z_valid_max]
-        for v in root_painter.viewers:
-            if v.isVisible():
-                v.update_seg_slice()
-                v.update_outline()
-        print('patch seg duration = ', time.time() - start_time)
+        self.seg_patch_thread = SegmentPatchThread(annot_patch, content,
+                                                   root_painter.server_ip,
+                                                   root_painter.server_port)
+        self.seg_patch_thread.complete.connect(partial(self.patch_received, bounded_fname=self.root_painter.bounded_fname))
+        self.seg_patch_thread.start()
+        print('after thread start')
