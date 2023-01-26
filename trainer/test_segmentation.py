0e637c37d0a562a5c312bfa01dc68773fde2390b
Abraham Smith <abe@abesmith.co.uk>
2023-01-26 10:35:37 +0100

    implemented basic test for segmentation output

--- trainer/test_segmentation.py
+++ trainer/test_segmentation.py
@@ -19,27 +19,24 @@ from trainer import Trainer
 from startup import startup_setup
 
 
-
 def test_segmentation_001():
-    from model_utils import segment_3d, random_model
-
+    from model_utils import segment_3d, random_model, pad_then_segment_3d
     image_shape = (72, 412, 412)  # depth, height, width, (z, y, x)
     image = np.random.random(image_shape)
     in_patch_shape = (64, 256, 256)
     out_patch_shape = (64-34, 256-34, 256-34)
     batch_size = 2
-    cnn = random_model(classes=['structure_of_interest'])
-    print('input image shape', image.shape)
-    # test segmentation can be generated with the same shape as an input image.
-    pred_maps = segment_3d(cnn, image, batch_size, in_patch_shape, out_patch_shape)
-    print('pred maps len = ', len(pred_maps))
-    print('pred maps[0].shape = ', pred_maps[0].shape)
-
+    classes = ['structure_of_interest']
+    cnn = random_model(classes)
+    pred_maps = pad_then_segment_3d(
+        cnn, image, batch_size,
+        in_patch_shape[1], out_patch_shape[1],
+        in_patch_shape[0], out_patch_shape[0])
+    assert len(pred_maps) == len(classes)
+    assert pred_maps[0].shape == image.shape
 
 
 def test_validation():
     # implement a very simple validation function.
     # it will go through all images in the validation set and get their dice.
     pass 
-
-
