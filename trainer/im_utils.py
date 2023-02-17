de05ecc494727ec2774d900d89d2807c550db9de
Abraham George Smith <abe@abesmith.co.uk>
2023-02-17 13:57:58 +0100

    removing workerid print

--- trainer/im_utils.py
+++ trainer/im_utils.py
@@ -240,9 +240,6 @@ def load_train_image_and_annot(dataset_dir, train_seg_dirs, train_annot_dirs, us
         if worker_info:
             worker_id = worker_info.id
             fnames = fnames[worker_id::worker_info.num_workers] 
-            print('fnames for ', worker_id, 'are', fnames)
-        else:
-            print("no worker info found")
 
         fname = random.sample(fnames, 1)[0]
 
