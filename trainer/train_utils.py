38cf47b04386c08e068f1ee7ab2aceb4dffbd9e0
Abraham Smith <abe@abesmith.co.uk>
2023-06-29 13:07:32 +0200

    adding val_epoch method to train_utils and linting

--- trainer/train_utils.py
+++ trainer/train_utils.py
@@ -14,20 +14,26 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+
+import time
+import copy
+
 import numpy as np
 import torch
+
 from patch_seg import handle_patch_update_in_epoch_step
 from model_utils import debug_memory
 from loss import get_batch_loss
 
 
-def epoch(model, classes, loader, batch_size,
+def train_epoch(model, classes, loader, batch_size,
           optimizer, patch_update_enabled=False,
           step_callback=None, stop_fn=None):
     """ One training epoch """
-    assert type(classes) == list, f'classes should be list, classes:{classes}'
+    assert isinstance(classes, list), f'classes should be list, classes:{classes}'
     model.train()
 
+    epoch_start = time.time()
     epoch_items_metrics = []  
     loss_sum = 0
     for step, (batch_im_patches, batch_fg_patches,
@@ -77,4 +83,78 @@ def epoch(model, classes, loader, batch_size,
             step_callback() # could update training parameter
         if stop_fn and stop_fn(): # in this context we consider validation part of training.
             return None # understood as training stopping early
+
+    duration = round(time.time() - epoch_start, 3)
+    print('')
+    print('Training epoch duration', duration, 'time per instance',
+          round((time.time() - epoch_start) / len(epoch_items_metrics), 3))
+
     return epoch_items_metrics
+
+
+
+
+def val_epoch(model, project_classes, dataset, val_patch_refs,
+              patch_update_enabled=False,
+              step_callback=None, stop_fn=None):
+    """
+    Compute the metrics for a given
+        model, annotation directory and dataset (image directory).
+    """
+    debug_memory('val epoch start')
+    epoch_start = time.time()
+    torch.set_grad_enabled(False)
+    fnames = {r.annot_fname for r in val_patch_refs}
+    epoch_items_metrics = []
+    step = 0
+
+    val_cnn = copy.deepcopy(model)
+    val_cnn.half()
+    for fname in fnames:
+        # We assume image has same extension as annotation.
+        # Is this always the case?
+        image = dataset.load_im(fname)
+        annots, classes = dataset.get_annots_for_image(fname)
+        refs = [r for r in val_patch_refs if r.annot_fname == fname]
+
+        for ref in refs:
+            (im_patch, fg_patches,
+             bg_patches, _segs) = dataset.get_patch_from_image(image, annots, ref)            
+            ignore_mask = ref.ignore_mask
+            
+            if step_callback:
+                step_callback()
+
+            batch_im_patches = torch.from_numpy(np.array([im_patch])).cuda()
+
+            if patch_update_enabled:
+                batch_im_patches = handle_patch_update_in_epoch_step(batch_im_patches, mode='val')
+
+            batch_im_patches = batch_im_patches.half()
+            outputs = val_cnn(batch_im_patches)
+            (_, batch_items_metrics) = get_batch_loss(
+                outputs,
+                [fg_patches],
+                [bg_patches],
+                [ignore_mask],
+                None,
+                [classes],
+                project_classes,
+                compute_loss=False)
+
+            epoch_items_metrics += batch_items_metrics
+            
+            if stop_fn and stop_fn():
+                return None # a way to stop validation quickly if user specifies
+
+            debug_memory('val epoch step')
+            # https://github.com/googlecolab/colabtools/issues/166
+            print(f"\rValidation: {(step+1)}/{len(val_patch_refs)}", end='', flush=True)
+            step += 1
+
+    duration = round(time.time() - epoch_start, 3)
+    print('')
+    print('Validation epoch duration', duration, 'time per instance',
+           round((time.time() - epoch_start) / len(epoch_items_metrics), 3))
+    return epoch_items_metrics
+
